2e7295c1483f7b6459afb3744d60ff81d77f7d5a
Richard <richard.knieper001@stud.fh-dortmund.de>
2021-07-20 17:45:08 +0200

    Parser implementation of subset of rules

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.StringReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
+        Parser parser = new Parser(new FileReader("src/example.fare"));
+        parser.cu();
     }
 }
